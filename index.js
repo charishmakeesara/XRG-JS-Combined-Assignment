edf4a2ab62d22d6735822ac694e79c51034b92c7
PREETHI DASARAPU <127194320+Preethigrace@users.noreply.github.com>
2023-04-14 16:16:20 +0530

    updates

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 // Structure of the Note:
 // {id: 1, title: 'Note 1', body: 'This is note 1', author: 'John Doe'}
 
-
 // Create Notes
-
-
-
 
 // Read Notes
 
-
-
-
 // Update Notes
-
-
-
-
+const updateNote = (note) => {
+  for (const curNote of notes) {
+    if (curNote.id == note.id) {
+      curNote.title = note.title;
+      curNote.body = note.body;
+      curNote.author = note.author;
+    }
+  }
+  return notes;
+};
 // Delete Notes
