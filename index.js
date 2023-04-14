1ab1e86e2922a1ea38927892bca206e800267769
saisantosh0208 <125540004+saisantosh0208@users.noreply.github.com>
2023-04-14 16:08:51 +0530

    reading notes

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 // Structure of the Note:
-// {id: 1, title: 'Note 1', body: 'This is note 1', author: 'John Doe'}
-
+const notes = [
+  { id: 1, title: 'Note 1', body: 'This is note 1', author: 'John Doe' },
+  { id: 2, title: 'Note 1', body: 'This is note 1', author: 'John Doe' },
+  { id: 3, title: 'Note 1', body: 'This is note 1', author: 'John Doe' },
+  { id: 4, title: 'Note 1', body: 'This is note 1', author: 'John Doe' },
+];
 
 // Create Notes
 
-
-
-
 // Read Notes
-
-
-
+const readnote = (id) => {
+  for (const note of notes) {
+    if (note.id == id);
+    {
+      console.log(notes_available);
+      return note;
+    }
+  }
+};
 
 // Update Notes
-
-
-
 
 // Delete Notes
