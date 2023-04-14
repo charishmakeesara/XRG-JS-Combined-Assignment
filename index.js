81dcfde875fec2c49d09aaf8d88a02ae8ac52886
Ilaiah Gudepu <ilaiah.g@xrgsolutions.com>
2023-04-14 16:09:31 +0530

    CreateNote funtion added

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 // Structure of the Note:
 // {id: 1, title: 'Note 1', body: 'This is note 1', author: 'John Doe'}
 
+// Create Notes;
 
-// Create Notes
+const notesArray = [];
 
+const addNoteObj = {
+  id: 1,
+  title: 'Note 123',
+  body: 'This is note 1',
+  author: 'John Doe',
+};
 
+const toCreateNote = (noteObj) => {
+  const hasSeen = {};
+  for (const notesValues of notesArray) {
+    hasSeen[notesValues.title] = true;
+  }
+  if (!hasSeen[noteObj.title]) {
+    noteObj.id = new Date().valueOf();
+    notesArray.push(noteObj);
+  }
+  return notesArray;
+};
 
-
+console.log(toCreateNote(addNoteObj));
 // Read Notes
 
-
-
-
 // Update Notes
-
-
-
 
 // Delete Notes
